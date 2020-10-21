c31172c1ffc8e56d9bf9dc6e42b1ce9b68b2393a
hungndph10602 <platevn1@gmail.com>
2020-10-21 13:17:24 +0700

    dal class

---
+++ 2.Project/QuanLyBanHang/DAL_QLBH/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_QLBH
+{
+    public class DbContext
+    {
+        public SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-M807K36\SQLEXPRESS;Initial Catalog=DuAnMau;Integrated Security=True");
+    }
+}

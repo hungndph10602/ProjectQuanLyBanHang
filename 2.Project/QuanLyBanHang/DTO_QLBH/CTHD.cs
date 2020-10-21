64f0209296836fadd3623e9919bde41f7c35c414
hungndph10602 <platevn1@gmail.com>
2020-10-21 10:51:29 +0700

    sua lai dto

--- 2.Project/QuanLyBanHang/DTO_QLBH/CTHD.cs
+++ 2.Project/QuanLyBanHang/DTO_QLBH/CTHD.cs
@@ -14,14 +14,14 @@ namespace DTO_QLBH
         public int macthd { get; set; }
         [ForeignKey("HoaDon")]
         public int mshd { get; set; }
-       
+
         [ForeignKey("sanphams")]
         public int mssp { get; set; }
-        
+
         public int soluong { get; set; }
         public double gia { get; set; }
 
         public virtual HoaDon HoaDon { get; set; }
-        public virtual ICollection<SanPham> sanphams { get; set; }
+        public virtual SanPham sanphams { get; set; }
     }
 }
